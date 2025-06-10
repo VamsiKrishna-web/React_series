8443a715ace78e3069b8271aab487b0a0661c4b2
kronosvamsi <beingaliver@gmail.com>
2025-06-10 19:24:59 +0530

    02Counter_update

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,62 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
   // let counter=6;
 
-  let [counter,setCounter]= useState(10)   /// hook for propgation of variable  update in ui
+  let [counter, setCounter] = useState(10); /// hook for propgation of variable  update in ui
 
-  let addValue  =   ()=>{
-
+  let addValue = () => {
     // console.log(counter);
-   
-    if (counter > 19){
 
+    if (counter > 19) {
       setCounter(20);
-
-
-    }
-    else{
+    } else {
       setCounter(counter + 1);
-
     }
-    
-    
-    
-  }
+  };
 
-  const removeValue = () =>{
-
+  const removeValue = () => {
     // console.log(counter)
-    if(counter < 1){
-
+    if (counter < 1) {
       setCounter(0);
-    }
-    else{
+    } else {
       setCounter(counter - 1);
     }
-
-    
-  }
+  };
 
   return (
     <>
-     
-    
-
-    <h1>React  with DOM</h1>
-    <h4>Counter :{counter}</h4>
-
-   <button onClick={addValue}> Add Value  : {counter}</button>
-    <br></br>
-    <button onClick={removeValue}>Remove Value  : {counter}</button>
-
-    <p>The value is {counter}</p>
+      <h1>React with DOM</h1>
+      <h4>Counter :{counter}</h4>
+      <button onClick={addValue}> Add Value : {counter}</button>
+      <br></br>
+      <button onClick={removeValue}>Remove Value : {counter}</button>
+      <p>The value is {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
